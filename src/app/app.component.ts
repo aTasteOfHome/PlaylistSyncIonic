edb60be079bfab8d988abbb34232c70fcc616d6a
andrewtsai <andrew.tsai@turner.com>
2018-05-23 12:08:22 -0400

    First half of Spotify auth implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,6 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    alert(page.title);
     this.nav.setRoot(page.component);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
@@ -21,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
